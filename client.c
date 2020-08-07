11b5caddb012b47e2cebdda06f823eb649e644b7
Jundong-chan <jundong.chen9807@gmail.com>
2020-08-07 22:10:55 +0800

    加上多线程控制读取数据和发送数据

--- client.c
+++ client.c
@@ -6,8 +6,9 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>      //hostent结构体定义在改头文件中
+#include <pthread.h>
 
-//下面给出hostent结构体的定义，该结构体用于存储域名解析返回的服务器基本信息
+//下面给出hostent结构体的定义
 /*struct hostent    
 { 
   char * h_name; / *主机的正式名称* / 
@@ -18,60 +19,120 @@
   #define h_addr h_addr_list [0] / *地址，以实现向后兼容* / 
 };*/
 
-
-void error(const char *msg)     //错误处理函数
+struct parament             //传给线程的参数
+{
+    int socketfd;
+    char *servername;
+    char *clientname;
+};
+struct parament init_pthread_parament(int fd,char *sername);
+void pthread_send(void *arg);
+void pthread_recv(void *arg);
+void error(const char *msg)
 {
     perror(msg);
     exit(0);
 }
-//运行时一共传三个参数，第一个是clinet，第二个是服务器的主机名，第三个是服务器的端口号
-int main(int argc, char *argv[])
+//客户端不需要直到自己建立连接的端口号，connect会自动分配，只需要服务器开放这个服务的端口号就可以
+int main(int argc, char *argv[])    //运行时一共传三个参数，第一个是clinet，第二个是服务器的主机名，第三个是服务器的端口号
 {
-    int sockfd, portno, n;  //sockfd是socket文件描述符，portno是服务器提供的端口
-    struct sockaddr_in serv_addr;
-    struct hostent *server;     //定义了hostent指针server,存储服务器的基本信息
-
+    int sockfd, portno;      //socket文件描述符，服务器提供的端口，
+    struct sockaddr_in serv_addr;   //sockaddr_in是包含了互联网地址的结构
+    struct hostent *server;     //hostent是host entry的缩写，记录了主机各种信息
+    
     char buffer[256];
-    if (argc < 3) {         //传入的参数少于3个，出错
+    pthread_t pthsend,pthrecv;  //两个线程，发送数据和接收数据
+
+    //传入的参数少于3个，出错
+    if (argc < 3) {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
     }
-    //1、创建一个socket
-    portno = atoi(argv[2]);     //将字符形式的端口号转成整数，并赋值
+    portno = atoi(argv[2]);     //第三个参数是端口号，将字符串转成数字
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
         error("ERROR opening socket");
-    
-    //2、域名解析过程（先查找本地hosts文件，找不到就发送DNS请求），参数是主机名，返回是一个hostent结构体
-    server = gethostbyname(argv[1]); 
+    server = gethostbyname(argv[1]);        //hostent结构是gethostbyname()函数的返回值，该函数做到了域名解析，并返回该域名主机的相关信息。
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
-    //3、将域名解析得到的信息给struct sockaddr_in serv_addr的成员
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;     //AF_INET表示IPV4协议
-    bcopy((char *)server->h_addr,       //复制ip地址给serv_addr.sin_addr.s_addr
+    bzero((char *) &serv_addr, sizeof(serv_addr));     //清空结构体里的数据
+    serv_addr.sin_family = AF_INET;     //AF_INET代表IPV4
+    bcopy((char *)server->h_addr,               //extern void bcopy(const void *src, void *dest, int n);函数原型，将参数1的前n个字节复制到参数2中
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
     serv_addr.sin_port = htons(portno);
-    //4、连接服务器
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) //connect()连接服务器
         error("ERROR connecting");
-    //5、写消息，传递
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    //6、读消息
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
-    //7、关闭socket连接
+    
+    struct parament para;
+
+    para=init_pthread_parament(sockfd,argv[1]);     //初始化多线程传入的参数的列表
+    int ret;
+    ret=pthread_create(&pthsend,NULL,(void *)pthread_send,(void*)&para);      
+    if(ret!=0)
+    {
+        perror("can't create send thread\n");
+    }
+    ret=pthread_create(&pthsend,NULL,(void *)pthread_recv,(void*)&para); 
+    if(ret!=0)
+    {
+        perror("can't create recev thread\n");
+    }
+
+    pthread_join(pthsend,NULL);      //等待子线程的结束
     close(sockfd);
     return 0;
+}
+
+void pthread_send(void *arg)
+{
+    struct parament par;
+    int n;
+    char buffer[256];  //缓冲区
+    par=*((struct parament *)arg);     //传入的参数转回原来的类型
+    printf("%s:welcome,please write your message\n",par.servername);
+    while(1)
+    {
+        //printf("chan:");
+        bzero(buffer,256);
+        buffer[0]='c';          //在消息头部放一个标志，s表示消息是服务器的
+        fgets(buffer+1,254,stdin);        //stdin实现对终端输入的数据进行读入操作
+        n = write(par.socketfd,buffer,strlen(buffer));    //写消息
+        if (n < 0) 
+            printf("ERROR writing to socket");
+    }
+}
+
+void pthread_recv(void *arg)
+{
+    struct parament par;    //主线程传进的参数结构
+    int n;
+    char buffer[256];  //缓冲区
+    par=*((struct parament *)arg);     //传入的参数转回原来的类型
+   
+    while(1)
+    {
+        bzero(buffer,256);
+        n = read(par.socketfd,buffer,255);
+        if (n < 0) 
+            error("ERROR reading from socket");
+        if(buffer[0]=='s')      //判断是从客户端发来的消息
+        {
+            printf("%s:%s\n",par.servername,buffer+1);
+        }
+    }
+}
+
+struct parament init_pthread_parament(int fd,char *sername)
+{
+    struct parament para;
+    char hostnambuff[256];      //本机主机名 
+    gethostname(hostnambuff,sizeof(hostnambuff));
+    para.clientname=hostnambuff;
+    para.servername=sername;
+    para.socketfd=fd;
+    return para;
+
 }
--- server.c
+++ server.c
@@ -1,70 +1,159 @@
 /* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
+   The port number is passed as an argument 
+   This version runs forever, forking off a separate 
+   process for each connection
+*/
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/socket.h>
-#include <netinet/in.h>  //该头文件定义了sockaddr_in结构
-//这是sockaddr_in结构的定义
+#include <netinet/in.h>
+#include <signal.h>
+#include <pthread.h>
 
-/*struct sockaddr_in 
-{ 
-  short sin_family; / *必须是AF_INET * / 
-  u_short sin_port; 
-  struct in_addr sin_addr; 
-  char sin_zero [8]; / *不使用，必须为零* / 
-};
-*/
-
-
-void error(const char *msg)     //错误处理函数
+void dostuff(int); /* function prototype */
+void waitchildsig(int signl);      //接受子进程发来的结束消息
+void pthread_recv(void *arg);
+void pthread_send(void *arg);
+void error(const char *msg)
 {
     perror(msg);
     exit(1);
 }
-
-int main(int argc, char *argv[])    //传入两个参数，默认，argv[0]是地址，argv[1]是端口号
-{                                           /*portno是服务器接受连接的端口号*/
-    int sockfd, newsockfd, portno;     /*sockfd文件描述符是该端口的主套接字，newsockfd是套接字建立连接后的返回值*/ 
-    socklen_t clilen;      //存储客户端地址的大小
-    char buffer[256];      //消息缓冲区
-    struct sockaddr_in serv_addr, cli_addr;     //sockaddr_in是包含了互联网地址的结构，
-    int n;                                //读写函数的文件描述符
-    if (argc < 2) {
-        fprintf(stderr,"ERROR, no port provided\n");
-        exit(1);
-    }                                           //创建一个套接字
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); //第一个参数表示套接字的地址域，AF_INET表示IPv4，第二个参数表示套接字的类型，SOCK_STREAM表示流套接字，对应可靠传输TCP
-    if (sockfd < 0)                            //第三个参数是协议，0表示由操作系统选择合适的协议。根据SOCK_STREAM，操作系统会选择TCP协议
-        error("ERROR opening socket");
-    
-    bzero((char *) &serv_addr, sizeof(serv_addr));      //将缓冲区的所有值设置为0
-    portno = atoi(argv[1]);                  //将传入的端口号参数从数字字符串转成整数
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;     //s_addr是long类型，存放服务器的IP地址，用宏定义INADDR_ANY获取
-    serv_addr.sin_port = htons(portno);         //将主机字节的顺序转化成网络字节顺序，htons()是将短整型数据转成大端格式（高位在低地址）
-    if (bind(sockfd, (struct sockaddr *) &serv_addr,    //bind()函数，将套接字和服务器地质结构绑定，一般出错原因会是该套接字已在计算机上使用
-        sizeof(serv_addr)) < 0){
-        error("ERROR on binding");
+void waitchildsig(int signl)
+{
+    if(signl==SIGCHLD)
+    {
+        waitpid(-1,NULL,WNOHANG);   //等待子进程的信号，WNOHANG表示非阻塞等待
     }
-        
-    listen(sockfd,5);       //监听该发送给该套接字上的连接请求，5表示该进程正在处理连接时，等待队列的最大限制数量，这也是大多数系统允许的最大数量
-    clilen = sizeof(cli_addr);
-    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);     //进程阻塞，直到客户端与服务器建立连接，返回一个新的对应该连接的套接字
-    if (newsockfd < 0) 
-        error("ERROR on accept");
-    
+}
+int main(int argc, char *argv[])
+{
+     int sockfd, newsockfd, portno, pid;
+     socklen_t clilen;
+     struct sockaddr_in serv_addr, cli_addr;
+
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+        sizeof(serv_addr)) < 0) 
+        error("ERROR on binding");
+     listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+     while (1){
+        newsockfd = accept(sockfd, 
+            (struct sockaddr *) &cli_addr, &clilen);
+        if(newsockfd < 0) 
+            error("ERROR on accept");
+        pid = fork();      //创建子进程
+        if(pid < 0)
+            error("ERROR on fork");
+        if(pid == 0){
+            //close(sockfd);
+            dostuff(newsockfd);
+            exit(0);
+        }
+        else{
+            signal(SIGCHLD,waitchildsig);   //通过特定函数处理该信号
+            close(newsockfd);
+        } 
+
+    } /* end of while */
+
+     close(sockfd);
+     return 0; /* we never get here */
+}
+
+/******** DOSTUFF() *********************
+ There is a separate instance of this function 
+ for each connection.  It handles all communication
+ once a connnection has been established.
+ *****************************************/
+void dostuff (int sock)
+{
+   int n;
+   int erro=0;
+   socklen_t len=sizeof(erro);
+   char buffer[256];
+   pthread_t send,receiv; 
+
+   int ret;
+   ret=pthread_create(&send,NULL,(void *)pthread_send,(void *)&sock);    //发送数据的子线程
+   if(ret!=0)
+    {
+        perror("can't create send thread\n");
+    }
+    ret=pthread_create(&receiv,NULL,(void *)pthread_recv,(void*)&sock); 
+    if(ret!=0)
+    {
+        perror("can't create recev thread\n");
+    }
+
+    pthread_join(send,NULL);    //等待子线程的结束
+
+   /*while(1getsockopt(sock, SOL_SOCKET, SO_ERROR, &erro, &len)==0)*/    //getsockopt=0,说明套接字正常工作
+   /*{
     bzero(buffer,256);
-    n = read(newsockfd,buffer,255);         //等待并读取客户端发来的信息
+    n = read(sock,buffer,255);
+    printf("Bob: %s\n",buffer+1); 
+    printf("%c",buffer[0]);
     if (n < 0) error("ERROR reading from socket");
-
-    printf("Here is the message: %s\n",buffer);
-    n = write(newsockfd,"I got your message",18);   //显示信息，18是信息的大小
+      
+    fgets(buffer,255,stdin);
+    n = write(sock,buffer,255);
     if (n < 0) error("ERROR writing to socket");
+   }*/
+  
+}
 
-    close(newsockfd);
-    close(sockfd);
-    return 0; 
+void pthread_send(void *arg)
+{
+    //struct parament par;
+    int n;
+    char buffer[256];  //缓冲区
+    int serverfd=*((int *)arg);
+    printf("chan:welcome,please write your message\n");
+    while(1/*getsockopt(clientfd, SOL_SOCKET, SO_ERROR, &erro, &len)==0*/)
+    {
+        //printf("chan:");
+        bzero(buffer,256);
+        buffer[0]='s';          //在消息头部放一个标志，s表示消息是服务器的
+        fgets(buffer+1,254,stdin);        //stdin实现对终端输入的数据进行读入操作
+        n = write(serverfd,buffer,strlen(buffer));    //写消息
+        if (n < 0) 
+            printf("ERROR writing to socket");
+    }
+}
+
+void pthread_recv(void *arg)
+{
+    char buffer[256];
+    int n=0;
+    int serverfd;
+    serverfd=*((int *)arg);     //传入的参数转回原来的类型
+    while(1)
+    {
+        bzero(buffer,256);
+        n = read(serverfd,buffer,255);
+        if (n < 0) 
+            error("ERROR reading from socket");
+        if(buffer[0]=='c')      //判断是从客户端发来的消息
+        {
+            printf("jundongchen:%s\n",buffer+1);
+        }
+    }
+
 }
